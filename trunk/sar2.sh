4869d8dadbab2155a6f2617cb1238ccd845d414b
hector1987 <hector1987@9e6900a4-42bf-4d36-85d2-5eeccc83afb8>
2011-02-28 22:30:54 +0000

    Updated quicklaunch script so it can be called from any location.

--- trunk/sar2.sh
+++ trunk/sar2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 BIN_FILE="bin/sar2"
+PATH_TO_GAME=$(dirname $0) #make it possible to execute this from any location
 
- if [[ ! -f $BIN_FILE ]]
+ if [[ ! -f "$PATH_TO_GAME/$BIN_FILE" ]]
  then
      echo "Search and Rescue II will be now compiled"
-     scons
+     "$PATH_TO_GAME/scons"
      if [[ $? -ne 0 ]]
      then 
          echo "Search and Rescue II cannot be compiled. Aborting..."
@@ -13,5 +14,6 @@ BIN_FILE="bin/sar2"
         echo "Search and Rescue II has been compiled successfully."
     fi
 fi
-export SEARCHANDRESCUE2_DATA="$(pwd)/data"
-bin/sar2
+# export full path of the data location
+export SEARCHANDRESCUE2_DATA="$(pwd)/$(dirname $0)/data"
+"$PATH_TO_GAME/bin/sar2"
