3f078eea974174e6d954b331b535f353495c83ff
hector1987 <hector1987@9e6900a4-42bf-4d36-85d2-5eeccc83afb8>
2011-03-05 23:29:01 +0000

    Added a script to create a tar file for distribution

---
+++ trunk/makedist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prepares Sar2 for distribution
+
+VERSION_STRING=$1
+
+if [ -z $VERSION_STRING ]
+then
+    echo "Usage ./makedist.sh VERSION-STRING"
+    exit 1
+fi
+
+echo "Cleaning up build files..."
+./cleanup.sh &>/dev/null
+echo "Packaging the game..."
+tar -zcf "sar2-$VERSION_STRING.tar.gz" --exclude ".*" --exclude-backups *
+if [[ $? -eq 0 ]]
+then
+    echo "Created sar2-$VERSION_STRING.tar.gz successfully."
+else
+    echo "Failed to create sar2 archive."
+fi
