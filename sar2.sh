8e9837c9b72159f3e281848780542c9ffc27a73b
Hector Sanjuan <hsanjuan@users.noreply.github.com>
2011-04-01 14:56:23 +0000

    Fixed path to scons in the quickstart file

--- sar2.sh
+++ sar2.sh
@@ -5,14 +5,16 @@ PATH_TO_GAME=$(dirname $0) #make it possible to execute this from any location
 if [[ ! -f "$PATH_TO_GAME/$BIN_FILE" ]]
  then
      echo "Search and Rescue II will be now compiled"
-     "$PATH_TO_GAME/scons"
+     cd "$PATH_TO_GAME"
+     scons
      if [[ $? -ne 0 ]]
      then 
          echo "Search and Rescue II cannot be compiled. Aborting..."
          exit 1
-    else
-        echo "Search and Rescue II has been compiled successfully."
-    fi
+     else
+         echo "Search and Rescue II has been compiled successfully."
+     fi
+     cd -
 fi
 # export full path of the data location
 export SEARCHANDRESCUE2_DATA="$(pwd)/$PATH_TO_GAME/data"
