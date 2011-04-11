0821383e220d4ee56a3f5ff5f57ca95cb5653337
hector1987 <hector1987@9e6900a4-42bf-4d36-85d2-5eeccc83afb8>
2011-04-11 23:22:14 +0000

    Somehow the makedist scripts were not updated

--- trunk/makedist.sh
+++ trunk/makedist.sh
@@ -3,6 +3,7 @@
 # Prepares Sar2 for distribution
 
 VERSION_STRING=$1
+NAME="sar2-$VERSION_STRING"
 
 if [ -z $VERSION_STRING ]
 then
@@ -10,13 +11,19 @@ then
     exit 1
 fi
 
+DISTFILES=$(ls)
+
 echo "Cleaning up build files..."
 scons -c &>/dev/null
 echo "Packaging the game..."
-tar -zcf "sar2-$VERSION_STRING.tar.gz" --exclude ".*" --exclude-backups *
-if [[ $? -eq 0 ]]
+mkdir $NAME
+cp -l -r $DISTFILES $NAME/
+tar -zcf $NAME.tar.gz --exclude-backups $NAME
+RES=$?
+rm -rf $NAME
+if [[ $RES -eq 0 ]]
 then
-    echo "Created sar2-$VERSION_STRING.tar.gz successfully."
+    echo "Created $NAME.tar.gz successfully."
 else
     echo "Failed to create sar2 archive."
 fi
