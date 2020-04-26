694cf63bfd511e913e85b6ae56247ea7055ffe5b
Hector Sanjuan <code@hector.link>
2020-04-26 14:22:34 +0200

    Improve makedist.sh

--- makedist.sh
+++ makedist.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Prepares Sar2 for distribution
+set -x
 
 VERSION_STRING=$1
-NAME="sar2-$VERSION_STRING"
+NAME="sar2-${VERSION_STRING}"
 
 if [ -z $VERSION_STRING ]
 then
@@ -11,16 +12,12 @@ then
     exit 1
 fi
 
-DISTFILES=$(ls)
-
 echo "Cleaning up build files..."
 scons -c &>/dev/null
+rm -f "$NAME"
 echo "Packaging the game..."
-mkdir $NAME
-cp -l -r $DISTFILES $NAME/
-tar -jcf $NAME.tar.bz2 --exclude-backups $NAME --exclude ".svn"
+tar -jcf "${NAME}.tar.bz2" --exclude-backups --exclude-vcs --exclude-vcs-ignores --{owner,group}=root --transform "s/\./$NAME/" .
 RES=$?
-rm -rf $NAME
 if [[ $RES -eq 0 ]]
 then
     echo "Created $NAME.tar.bz2 successfully."
